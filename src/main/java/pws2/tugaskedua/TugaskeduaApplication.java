add3a65b4aaf4c5f8d8f74857f983c5ce14101e4
dhiastii <dhiastiputri.jayanti@gmail.com>
2022-10-29 10:09:51 +0700

    First commit

---
+++ src/main/java/pws2/tugaskedua/TugaskeduaApplication.java
@@ -0,0 +1,13 @@
+package pws2.tugaskedua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugaskeduaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugaskeduaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pws2/tugaskedua/TugaskeduaApplicationTests.java
@@ -0,0 +1,13 @@
+package pws2.tugaskedua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugaskeduaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
